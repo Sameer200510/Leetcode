40085b2f91d6031d44e25beb2067f7ebcbe5b5a0
SAMEER LOHANI <sameerlohani200510@gmail.com>
2025-03-03 19:41:30 +0530

    Time: 6 ms (18.77%), Space: 8.7 MB (41.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,11 +1,15 @@
-class Solution {
+class Solution 
+{
 public:
-    bool isPalindrome(int x) {
-       if (x < 0 || (x % 10 == 0 && x != 0)) {
+    bool isPalindrome(int x) 
+    {
+       if (x < 0 || (x % 10 == 0 && x != 0)) 
+        {
             return false;
         }
         int revertedNumber = 0;
-        while (x > revertedNumber) {
+        while (x > revertedNumber) 
+        {
             revertedNumber = revertedNumber * 10 + x % 10;
             x /= 10;
         }
