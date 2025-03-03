b6aa0942ff9dacc5a58aae3d3040ac9366c1ea1a
SAMEER LOHANI <sameerlohani200510@gmail.com>
2025-03-03 19:59:34 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.71%) - LeetHub

--- 0027-remove-element/0027-remove-element.c
+++ 0027-remove-element/0027-remove-element.c
@@ -1,17 +1,21 @@
 int removeElement(int* nums, int numsSize, int val) 
 {
     int k=0;
-    for(int i=0;i<numsSize;i++){
-        if(nums[i]!=val){
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=val)
+        {
             nums[k]=nums[i];
             k++;
         }
        
     }
-    
-    for(int i=1;i<k;i++){
-        for(int j=0;j<k-i;j++){
-            if(nums[j]>nums[j+1]){
+    for(int i=1;i<k;i++)
+    {
+        for(int j=0;j<k-i;j++)
+        {
+            if(nums[j]>nums[j+1])
+            {
                 int t=nums[j];
                 nums[j]=nums[j+1];
                 nums[j+1]=t;
