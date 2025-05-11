b5f2bb8df5ef96172354d9c6360a33b58d901054
SAMEER LOHANI <sameerlohani200510@gmail.com>
2025-05-11 11:32:42 +0530

    Time: 0 ms (100%), Space: 12 MB (1.15%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) 
     {
-        int c=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]%2!=0){
-                c++;
-                if(c==3){
-                    return true;
-                }
-            }else{
-                c=0;
+        int count=0;
+        if(arr.size()==2||arr.size()==1) return false;
+        for(int i=0;i<arr.size()-2;i++)
+        {
+            if(arr[i]%2!=0&&arr[i+1]%2!=0&&arr[i+2]%2!=0)
+            {
+                return true;
             }
         }
         return false;
